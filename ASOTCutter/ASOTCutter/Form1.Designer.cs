3ae96c9cd50e1e88b03de3383d76d2346fa854e3
Хованский Владимир <vkhovanskiy@gmail.com>
2018-03-22 16:11:07 +0200

    Start Project

---
+++ ASOTCutter/ASOTCutter/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ASOTCutter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDirPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ButtonDirPath = new System.Windows.Forms.Button();
+            this.textBoxFormat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ButtonStartStop = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tempstat = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusTextLable = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxDirPath
+            // 
+            this.textBoxDirPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDirPath.Location = new System.Drawing.Point(67, 12);
+            this.textBoxDirPath.Name = "textBoxDirPath";
+            this.textBoxDirPath.Size = new System.Drawing.Size(355, 20);
+            this.textBoxDirPath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Directory:";
+            // 
+            // ButtonDirPath
+            // 
+            this.ButtonDirPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonDirPath.Location = new System.Drawing.Point(428, 10);
+            this.ButtonDirPath.Name = "ButtonDirPath";
+            this.ButtonDirPath.Size = new System.Drawing.Size(75, 23);
+            this.ButtonDirPath.TabIndex = 2;
+            this.ButtonDirPath.Text = "Open";
+            this.ButtonDirPath.UseVisualStyleBackColor = true;
+            this.ButtonDirPath.Click += new System.EventHandler(this.ButtonDirPath_Click);
+            // 
+            // textBoxFormat
+            // 
+            this.textBoxFormat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFormat.Location = new System.Drawing.Point(67, 39);
+            this.textBoxFormat.Name = "textBoxFormat";
+            this.textBoxFormat.Size = new System.Drawing.Size(355, 20);
+            this.textBoxFormat.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Format:";
+            // 
+            // ButtonStartStop
+            // 
+            this.ButtonStartStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonStartStop.Location = new System.Drawing.Point(429, 35);
+            this.ButtonStartStop.Name = "ButtonStartStop";
+            this.ButtonStartStop.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStartStop.TabIndex = 5;
+            this.ButtonStartStop.Text = "Start";
+            this.ButtonStartStop.UseVisualStyleBackColor = true;
+            this.ButtonStartStop.Click += new System.EventHandler(this.ButtonStartStop_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tempstat,
+            this.StatusTextLable});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 98);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(515, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tempstat
+            // 
+            this.tempstat.Name = "tempstat";
+            this.tempstat.Size = new System.Drawing.Size(42, 17);
+            this.tempstat.Text = "Status:";
+            // 
+            // StatusTextLable
+            // 
+            this.StatusTextLable.Name = "StatusTextLable";
+            this.StatusTextLable.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 120);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.ButtonStartStop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxFormat);
+            this.Controls.Add(this.ButtonDirPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDirPath);
+            this.Name = "Form1";
+            this.Text = "ASOT Cutter";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDirPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ButtonDirPath;
+        private System.Windows.Forms.TextBox textBoxFormat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ButtonStartStop;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tempstat;
+        private System.Windows.Forms.ToolStripStatusLabel StatusTextLable;
+    }
+}
+
---
+++ ASOTCutter/ASOTCutter/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASOTCutter
+{
+    public partial class Form1 : Form
+    {
+        static Regex reg = new Regex(@".+?\s*TRACK\s*(?<TRACK>\d+)\s*.+?PERFORMER\s*\""(?<PERFORMER>.+?)\"".+?TITLE\s*\""(?<TITLE>.+?)\"".+?INDEX\s*01\s*(?<INDEX>\d+:\d+:\d+)", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Singleline);
+        static Regex regNum = new Regex(@"\d*", RegexOptions.Compiled);
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxDirPath.Text = @"D:\MUSIC\ ASOT\700-799\725";
+            textBoxFormat.Text = @"[ASOT %DIR_NAME%] %TRACK%. %PERFORMER% - %TITLE%";
+        }
+
+        private void ButtonDirPath_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBoxDirPath.Text = fbd.SelectedPath;
+                    //string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+            }
+        }
+
+        private void ButtonStartStop_Click(object sender, EventArgs e)
+        {
+            StatusTextLable.Text = "";
+            if (textBoxDirPath.Text.IsNullOrEmpty() && !Directory.Exists(textBoxDirPath.Text))
+            {
+                StatusTextLable.Text = @"Directory Is Null Or Not Exist!";
+                return;
+            }
+
+            FindAllSubDir(textBoxDirPath.Text);
+        }
+
+        void FindAllSubDir(string dirPath)
+        {
+            string[] DirPaths = Directory.GetDirectories(dirPath, "*", SearchOption.AllDirectories);
+            foreach (var dir in DirPaths)
+            {
+                FindAllSubDir(dir);
+            }
+            GetCueAndMp3(dirPath);
+        }
+
+        void GetCueAndMp3(string path)
+        {
+            string[] files = Directory.GetFiles(path, "*.cue");
+            foreach (string fileCuePath in files)
+            {
+                string fileName = fileCuePath.Substring(0, fileCuePath.Length - 4);
+                if (File.Exists(fileName + ".mp3"))
+                {
+                    string dirName = Path.GetFileName(path);
+                    List<Track>  tracks = ReadCue(dirName, fileCuePath);
+
+                    foreach (Track track in tracks)
+                    {
+                        string outputTrackResult = Path.Combine(path, "Result", track.TrackFileName + ".mp3");
+
+                        //TrimMp3(fileName + ".mp3", outputTrackResult, TimeSpan.FromMinutes(2), TimeSpan.FromMinutes(2.5));
+                    }
+                }
+            }
+        }
+
+        class Track
+        {
+            public string TrackFileName { get; set; }
+            public TimeSpan Start { get; set; }
+        }
+        List<Track> ReadCue(string parentDirName, string cuePath)
+        {
+            List<Track> SetTracks = new List<Track>();
+            File.SetAttributes(cuePath, FileAttributes.Normal);
+            using (FileStream inputStream = File.OpenRead(cuePath))
+            {
+                using (StreamReader inputReader = new StreamReader(inputStream))
+                {
+                    string cueData = inputReader.ReadToEnd();
+                    MatchCollection collection = reg.Matches(cueData);
+                    foreach (Match track in collection)
+                    {
+                        if (track.Groups.Count > 0)
+                        {
+                            Track newTrack = new Track();
+                            newTrack.TrackFileName = textBoxFormat.Text;
+
+                            foreach (string match in reg.GetGroupNames())
+                            {
+                                if(match == "0")
+                                    continue;
+
+                                newTrack.TrackFileName =  new Regex(string.Format(@"%\s*{0}\s*%", match), RegexOptions.IgnoreCase).Replace(newTrack.TrackFileName, track.Groups[match].Value);
+                            }
+
+                            newTrack.TrackFileName = new Regex(string.Format(@"%\s*{0}\s*%", "DIR_NAME"), RegexOptions.IgnoreCase).Replace(newTrack.TrackFileName, regNum.Match(parentDirName).Value);
+
+                            TimeSpan spanMin = TimeSpan.FromMinutes(int.Parse(track.Groups["INDEX"].Value.Split(':')[0]));
+                            TimeSpan spanSec = TimeSpan.FromSeconds(int.Parse(track.Groups["INDEX"].Value.Split(':')[1]));
+                            //TimeSpan spanMilisec = TimeSpan.FromMilliseconds(int.Parse(track.Groups["INDEX"].Value.Split(':')[2]));
+                            newTrack.Start = spanMin + spanSec;
+
+                            SetTracks.Add(newTrack);
+                        }
+                    }
+                }
+            }
+            return SetTracks;
+        }
+
+        //void TrimMp3(string inputPath, string outputPath, TimeSpan? begin, TimeSpan? end)
+        //{
+        //    if (begin.HasValue && end.HasValue && begin > end)
+        //        throw new ArgumentOutOfRangeException("end", "end should be greater than begin");
+
+        //    using (var reader = new Mp3FileReader(inputPath))
+        //    using (var writer = File.Create(outputPath))
+        //    {
+        //        Mp3Frame frame;
+        //        while ((frame = reader.ReadNextFrame()) != null)
+        //            if (reader.CurrentTime >= begin || !begin.HasValue)
+        //            {
+        //                if (reader.CurrentTime <= end || !end.HasValue)
+        //                    writer.Write(frame.RawData, 0, frame.RawData.Length);
+        //                else break;
+        //            }
+        //    }
+        //}
+    }
+}
