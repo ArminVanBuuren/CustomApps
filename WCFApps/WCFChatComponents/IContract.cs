8a2d756de7d6ae8d009897426b897824c3b6eeca
Хованский Владимир <vkhovanskiy@gmail.com>
2018-07-23 23:10:07 +0300

    wdwdw

---
+++ WCFApps/WCFChatComponents/IContract.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFChatComponents
+{
+    [ServiceContract]
+    interface IContract
+    {
+        [OperationContract]
+        void Login(string guid);
+
+        [OperationContract]
+        void Say(string input);
+
+        [OperationContract]
+        void Logoff();
+    }
+}
