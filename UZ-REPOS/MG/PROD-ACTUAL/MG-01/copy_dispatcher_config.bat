4f10167c48598372ddde47024b57eff8f038d213
vhovanskij <vkhovanskiy@gmail.com>
2018-12-05 13:09:41 +0300

    add mg actual change catalog config

---
+++ UZ-REPOS/MG/PROD-ACTUAL/MG-01/copy_dispatcher_config.bat
@@ -0,0 +1,17 @@
+xcopy /e/y/z "C:\FORIS\Messaging Gateway\Dispatcher\01_01\XSLT\Scripts" "C:\FORIS\Messaging Gateway\Dispatcher\01_02\XSLT\Scripts"
+xcopy /e/y/z "C:\FORIS\Messaging Gateway\Dispatcher\01_01\XSLT\Scripts" "C:\FORIS\Messaging Gateway\Dispatcher\01_03\XSLT\Scripts"
+xcopy /e/y/z "C:\FORIS\Messaging Gateway\Dispatcher\01_01\XSLT\Scripts" "C:\FORIS\Messaging Gateway\Dispatcher\01_04\XSLT\Scripts"
+
+
+xcopy /e/y/z "C:\FORIS\Messaging Gateway\Dispatcher\01_01\XSLT\Scripts" "\\a14-mg02\c$\\FORIS\Messaging Gateway\Dispatcher\01_01\XSLT\Scripts"
+xcopy /e/y/z "C:\FORIS\Messaging Gateway\Dispatcher\01_01\XSLT\Scripts" "\\a14-mg02\c$\\FORIS\Messaging Gateway\Dispatcher\01_02\XSLT\Scripts"
+xcopy /e/y/z "C:\FORIS\Messaging Gateway\Dispatcher\01_01\XSLT\Scripts" "\\a14-mg02\c$\\FORIS\Messaging Gateway\Dispatcher\01_03\XSLT\Scripts"
+xcopy /e/y/z "C:\FORIS\Messaging Gateway\Dispatcher\01_01\XSLT\Scripts" "\\a14-mg02\c$\\FORIS\Messaging Gateway\Dispatcher\01_04\XSLT\Scripts"
+
+xcopy /e/y/z "C:\FORIS\Messaging Gateway\Dispatcher\01_01\XSLT\Scripts" "\\a14-mg03\c$\\FORIS\Messaging Gateway\Dispatcher\03_01\XSLT\Scripts"
+xcopy /e/y/z "C:\FORIS\Messaging Gateway\Dispatcher\01_01\XSLT\Scripts" "\\a14-mg03\c$\\FORIS\Messaging Gateway\Dispatcher\03_02\XSLT\Scripts"
+xcopy /e/y/z "C:\FORIS\Messaging Gateway\Dispatcher\01_01\XSLT\Scripts" "\\a14-mg03\c$\\FORIS\Messaging Gateway\Dispatcher\03_03\XSLT\Scripts"
+xcopy /e/y/z "C:\FORIS\Messaging Gateway\Dispatcher\01_01\XSLT\Scripts" "\\a14-mg03\c$\\FORIS\Messaging Gateway\Dispatcher\03_04\XSLT\Scripts"
+
+
+pause
