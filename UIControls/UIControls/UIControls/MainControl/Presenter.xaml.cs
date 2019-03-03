27881e2739c457af099e2dc49ac6a6f7c52dec2b
Хованский Владимир <vkhovanskiy@gmail.com>
2019-03-03 22:18:35 +0300

    add path

--- UIControls/UIControls/UIControls/MainControl/Presenter.xaml.cs
+++ UIControls/UIControls/UIControls/MainControl/Presenter.xaml.cs
@@ -16,7 +16,8 @@ namespace UIControls.MainControl
         public Presenter(string textPresenter = null, bool canDragMove = true, bool panelItemIsVisible = true) : base(canDragMove, panelItemIsVisible)
         {
             InitializeComponent();
-            this.Icon = UIControls.Properties.Resources.overwolf.ToImageSource();
+            //this.Icon = UIControls.Properties.Resources.overwolf.ToImageSource();
+            
             if (!string.IsNullOrEmpty(textPresenter))
             {
                 X_title.Text = X_title.Text + "\r\n" + textPresenter;
