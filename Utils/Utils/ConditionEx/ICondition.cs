58fb5fb73630f34221b6d46b49dfee47ac1cace9
Хованский Владимир <vkhovanskiy@gmail.com>
2019-05-27 19:16:55 +0300

    1

--- Utils/Utils/ConditionEx/ICondition.cs
+++ Utils/Utils/ConditionEx/ICondition.cs
@@ -8,7 +8,14 @@ namespace Utils.ConditionEx
 {
     public interface ICondition
     {
+        /// <summary>
+        /// Результат вычисления вырожения
+        /// </summary>
         bool ConditionResult { get; }
+        
+        /// <summary>
+        /// Текстовый результат логических группировок вырожения
+        /// </summary>
         string StringResult { get; }
     }
 }
--- Utils/Utils/ConditionEx/IExpression.cs
+++ Utils/Utils/ConditionEx/IExpression.cs
@@ -9,6 +9,10 @@ namespace Utils.ConditionEx
     public interface IExpression
     {
         Expression Parent { get; }
+        
+        /// <summary>
+        /// Проверяется валидность самого вырожения
+        /// </summary>
         bool IsValid { get; }
     }
 }
