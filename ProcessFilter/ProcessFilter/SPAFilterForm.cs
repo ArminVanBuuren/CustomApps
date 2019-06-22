9806afba514ae09a1e6b2207e6d91a7162b687d6
Хованский Владимир <vkhovanskiy@gmail.com>
2019-06-22 17:10:13 +0300

    1

--- ProcessFilter/ProcessFilter/SPAFilterForm.cs
+++ ProcessFilter/ProcessFilter/SPAFilterForm.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -10,7 +9,6 @@ using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
-using System.Threading;
 using System.Windows.Forms;
 using System.Xml;
 using System.Threading.Tasks;
