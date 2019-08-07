cc80b911541d9379e5aa3c67a5b52669aee878f5
Хованский Владимир <vkhovanskiy@gmail.com>
2019-08-07 22:09:48 +0300

    1

--- Utils/Tester.UIControls/MainWindow.xaml.cs
+++ Utils/Tester.UIControls/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
@@ -61,7 +62,7 @@ namespace Tester.UIControls
             }
             finally
             {
-                Close();
+                Process.GetCurrentProcess().Kill();
             }
         }
 
