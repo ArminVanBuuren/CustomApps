ad712dae56d56ec67a5c03f5bc0b7fcbff9d350e
Хованский Владимир <vkhovanskiy@gmail.com>
2019-09-16 11:32:12 +0300

    1

---
+++ Minsk-REPOS/SPA/PROD-2019.09.01/SPA.SA/scp155/Dictionaries/Func/ifstringformat_scp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FORIS.ScenarioProcessing.Base.Utilities.Functions;
+
+namespace FORIS.ScenarioProcessing.Utilities.Functions
+{
+    public class ifstringformat_scp : IDictionaryFunction
+    {
+        /// <summary>
+        /// Выбор по условию из HLA Bug-524675. Некорректное формирование кода группы ЗГП
+        /// </summary>
+        /// <param name="args">параметры. Пример: ifstringformat_scp(conditionValue, postValue)</param>
+        /// <returns>conditionValue, если не пустой. Иначе postValue</returns>
+        public String Invoke(params object[] args)
+        {
+            string conditionValue = args[0].ToString();
+            string postValue = args[1].ToString();
+
+            return conditionValue != ""
+                       ? conditionValue
+                       : postValue;
+        }
+    }
+}
