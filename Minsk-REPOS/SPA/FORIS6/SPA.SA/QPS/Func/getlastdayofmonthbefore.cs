2259313fbcf1d7ff98c9c047165b3ae2d8f21237
Хованский Владимир <vkhovanskiy@gmail.com>
2019-10-10 20:00:26 +0300

    BEB

---
+++ Minsk-REPOS/SPA/FORIS6/SPA.SA/QPS/Func/getlastdayofmonthbefore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml;
+using FORIS.ScenarioProcessing.Base.Utilities.Functions;
+
+namespace FORIS.ScenarioProcessing.Utilities.Functions
+{
+    public class getlastdayofmonthbefore : IDictionaryFunction
+    {
+        /// <summary>
+        /// Вернуть последний день текущего месяца
+        /// </summary>
+        public String Invoke(XmlDocument request, params object[] args)
+        {
+            //DateTime date = DateTime.Now;
+            // DateTime lastDayOfMonth = new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month), 23, 59, 59);
+			
+			DateTime dateMinusMonth = DateTime.Today.AddMonths(-1);
+			DateTime lastDayMonthBefore = new DateTime(dateMinusMonth.Year, dateMinusMonth.Month, DateTime.DaysInMonth(dateMinusMonth.Year, dateMinusMonth.Month));
+            //return lastDayOfMonth.ToString("yyyy.MM.dd HH:mm:ss");
+			return lastDayMonthBefore.ToString("yyyy-MM-dd");
+        }
+    }
+}
