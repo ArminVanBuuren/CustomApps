74a1eb8ba7d98739429f9e3ad829b2216a5eca2b
Хованский Владимир <vkhovanskiy@gmail.com>
2020-02-06 16:09:54 +0300

    1

---
+++ Minsk-REPOS/SPA/PROD-2020.02.06/SPA.SA/QPS/dictionaries/Func/getyesterday.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Xml;
+using FORIS.ScenarioProcessing.Base.Utilities.Functions;
+
+namespace FORIS.ScenarioProcessing.Utilities.Functions
+{
+    public class getyesterday : IDictionaryFunction
+    {
+        /// <summary>
+        /// Вернуть последний день текущего месяца
+        /// </summary>
+        public String Invoke(params object[] args)
+        {
+            return DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
+        }
+    }
+}
