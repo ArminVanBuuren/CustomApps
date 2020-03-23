2e4c8b48507f7d7b2537ecb06c1e4d97abd0f63d
Хованский Владимир <vkhovanskiy@gmail.com>
2020-03-23 15:18:37 +0300

    1

---
+++ Utils/Utils/STREAM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class STREAM
+    {
+        public static void GarbageCollect()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                GC.WaitForPendingFinalizers();
+                GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+            }
+            GC.GetTotalMemory(true);
+        }
+    }
+}
