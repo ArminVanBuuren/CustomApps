e560fe4da510f0015941b7fbf26f3d13c5108148
Хованский Владимир <vkhovanskiy@gmail.com>
2020-04-16 03:23:03 +0300

    1

--- Utils/Utils.UIControls/Main/Presenter.xaml.cs
+++ Utils/Utils.UIControls/Main/Presenter.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace Utils.UIControls.Main
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Utils.UIControls.Main
 {
     public partial class Presenter
     {
@@ -16,5 +20,32 @@
             //Uri uriIcon = new Uri(@"C:\@MyRepos\CustomApp\UIControls\UIControls\UIControls\Images\overwolf.ico", UriKind.RelativeOrAbsolute);
             //this.Icon = new BitmapImage(uriIcon);
         }
+
+        public static void ShowOwner()
+        {
+            var vkhovanskiy = new Presenter(string.Empty, false, false)
+            {
+                Width = SystemParameters.PrimaryScreenWidth,
+                Height = SystemParameters.PrimaryScreenHeight,
+                Topmost = true,
+                Top = 0,
+                Left = 0,
+                WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen
+            };
+            vkhovanskiy.Loaded += WindowInfo_Loaded;
+            vkhovanskiy.ShowDialog();
+            vkhovanskiy.Loaded -= WindowInfo_Loaded;
+        }
+
+        private static void WindowInfo_Loaded(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (UIWindow)sender;
+            var infButtom = (Border)mainWindow.Template.FindName("TitleBar", mainWindow);
+            var infText = (TextBlock)mainWindow.Template.FindName("Caption", mainWindow);
+            infButtom.Background = (Brush)mainWindow.FindResource("AuthorWindowTopBorderBrush");
+            infText.Foreground = (Brush)mainWindow.FindResource("AuthorWindowTopBorderTextBrush");
+            infText.Opacity = 1;
+        }
+
     }
 }
