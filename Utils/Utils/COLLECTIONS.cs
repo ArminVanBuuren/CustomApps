12a20c53cd9caa158df789d0e6e7cb8061a82a45
Хованский Владимир <vkhovanskiy@gmail.com>
2020-04-20 07:30:55 +0300

    1

--- Utils/Utils/COLLECTIONS.cs
+++ Utils/Utils/COLLECTIONS.cs
@@ -91,21 +91,6 @@ namespace Utils
             );
         }
 
-        public static IOrderedQueryable<T> OrderByDescending<T>(this IQueryable<T> query, string memberName)
-        {
-            ParameterExpression[] typeParams = new ParameterExpression[] { Expression.Parameter(typeof(T), "") };
-            var pi = typeof(T).GetProperty(memberName);
-
-            return (IOrderedQueryable<T>)query.Provider.CreateQuery(
-                Expression.Call(
-                    typeof(Queryable),
-                    "OrderByDescending",
-                    new Type[] { typeof(T), pi.PropertyType },
-                    query.Expression,
-                    Expression.Lambda(Expression.Property(typeParams[0], pi), typeParams))
-            );
-        }
-
         public static IOrderedQueryable<T> ThenBy<T>(this IOrderedQueryable<T> query, string memberName)
         {
             ParameterExpression[] typeParams = new ParameterExpression[] { Expression.Parameter(typeof(T), "") };
@@ -117,6 +102,21 @@ namespace Utils
                 Expression.Call(
                     typeof(Queryable),
                     "ThenBy",
+                    new Type[] { typeof(T), pi.PropertyType },
+                    query.Expression,
+                    Expression.Lambda(Expression.Property(typeParams[0], pi), typeParams))
+            );
+        }
+
+        public static IOrderedQueryable<T> OrderByDescending<T>(this IQueryable<T> query, string memberName)
+        {
+            ParameterExpression[] typeParams = new ParameterExpression[] { Expression.Parameter(typeof(T), "") };
+            var pi = typeof(T).GetProperty(memberName);
+
+            return (IOrderedQueryable<T>)query.Provider.CreateQuery(
+                Expression.Call(
+                    typeof(Queryable),
+                    "OrderByDescending",
                     new Type[] { typeof(T), pi.PropertyType },
                     query.Expression,
                     Expression.Lambda(Expression.Property(typeParams[0], pi), typeParams))
