871ca3e5bd4f069cc231a2d9cd569b07c57c11c0
Хованский Владимир <vkhovanskiy@gmail.com>
2020-05-13 00:00:21 +0300

    1

--- SPAMassageSaloon/LogsReader/Reader/LogsReaderPerformer.cs
+++ SPAMassageSaloon/LogsReader/Reader/LogsReaderPerformer.cs
@@ -137,7 +137,7 @@ namespace LogsReader.Reader
 	                if (!folderMatch.Success)
 		                throw new Exception(string.Format(Resources.Txt_Forms_FolderIsIncorrect, originalFolder.Key));
 
-                    var serverFolder = string.Format($"\\\\{0}\\{folderMatch.Groups["DISC"]}${folderMatch.Groups["FULL"]}", serverName);
+                    var serverFolder = string.Format($"\\\\{0}\\{folderMatch.Groups["DISC"]}$\\{folderMatch.Groups["FULL"]}", serverName);
 	                if (!Directory.Exists(serverFolder))
 		                continue;
 
