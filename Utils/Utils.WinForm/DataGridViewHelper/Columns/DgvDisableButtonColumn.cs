a4e863ef93e5a55a9834c53786bf867fee9bfbeb
Хованский Владимир <vkhovanskiy@gmail.com>
2020-08-03 21:40:20 +0300

    1

---
+++ Utils/Utils.WinForm/DataGridViewHelper/Columns/DgvDisableButtonColumn.cs
@@ -0,0 +1,116 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Utils.WinForm.DataGridViewHelper
+{
+	[ToolboxBitmap(typeof(DataGridViewButtonColumn))]
+	public class DgvDisableButtonColumn : DataGridViewButtonColumn
+	{
+		public sealed override DataGridViewCell CellTemplate
+		{
+			get => base.CellTemplate;
+			set => base.CellTemplate = value;
+		}
+
+		public DgvDisableButtonColumn()
+		{
+			base.CellTemplate = new DgvDisableButtonCell();
+		}
+
+		public override object Clone()
+		{
+			if (base.Clone() is DgvDisableButtonColumn c)
+			{
+				((DgvDisableButtonCell)c.CellTemplate).Enabled = ((DgvDisableButtonCell)CellTemplate).Enabled;
+				return c;
+			}
+
+			return null;
+		}
+	}
+
+	public class DgvDisableButtonCell : DataGridViewButtonCell
+	{
+		private bool enabledValue;
+
+		public bool Enabled
+		{
+			get => enabledValue;
+			set
+			{
+				if (enabledValue == value)
+					return;
+				enabledValue = value;
+				// force the cell to be re-painted
+				DataGridView?.InvalidateCell(this);
+			}
+		}
+
+		public DgvDisableButtonCell()
+		{
+			enabledValue = true;
+		}
+
+		protected override void Paint(Graphics graphics, Rectangle clipBounds, Rectangle cellBounds, int rowIndex,
+			DataGridViewElementStates elementState, object value, object formattedValue, string errorText,
+			DataGridViewCellStyle cellStyle,
+			DataGridViewAdvancedBorderStyle advancedBorderStyle, DataGridViewPaintParts paintParts)
+		{
+			// The button cell is disabled, so paint the border, background, and disabled button for the cell. 
+			if (!enabledValue)
+			{
+				var currentContext = BufferedGraphicsManager.Current;
+
+				using (var myBuffer = currentContext.Allocate(graphics, cellBounds))
+				{
+					// Draw the cell background, if specified. 
+					if ((paintParts & DataGridViewPaintParts.Background) == DataGridViewPaintParts.Background)
+					{
+						using (var cellBackground = new SolidBrush(cellStyle.BackColor))
+						{
+							myBuffer.Graphics.FillRectangle(cellBackground, cellBounds);
+						}
+					}
+
+					// Draw the cell borders, if specified. 
+					if ((paintParts & DataGridViewPaintParts.Border) == DataGridViewPaintParts.Border)
+					{
+						PaintBorder(myBuffer.Graphics, clipBounds, cellBounds, cellStyle, advancedBorderStyle);
+					}
+
+					// Calculate the area in which to draw the button.
+					var buttonArea = cellBounds;
+					var buttonAdjustment = BorderWidths(advancedBorderStyle);
+					buttonArea.X += buttonAdjustment.X;
+					buttonArea.Y += buttonAdjustment.Y;
+					buttonArea.Height -= buttonAdjustment.Height;
+					buttonArea.Width -= buttonAdjustment.Width;
+
+					// Draw the disabled button.                
+					ButtonRenderer.DrawButton(myBuffer.Graphics, buttonArea, PushButtonState.Disabled);
+
+					// Draw the disabled button text.  
+					if (FormattedValue is string formattedValueString)
+					{
+						TextRenderer.DrawText(myBuffer.Graphics, formattedValueString, DataGridView.Font, buttonArea, SystemColors.GrayText, TextFormatFlags.PreserveGraphicsTranslateTransform | TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+					}
+
+					myBuffer.Render();
+				}
+			}
+			else
+			{
+				// The button cell is enabled, so let the base class handle the painting. 
+				base.Paint(graphics, clipBounds, cellBounds, rowIndex, elementState, value, formattedValue, errorText, cellStyle, advancedBorderStyle, paintParts);
+			}
+		}
+
+		public override object Clone()
+		{
+			var cell = (DgvDisableButtonCell)base.Clone();
+			cell.Enabled = Enabled;
+			return cell;
+		}
+	}
+}
---
+++ Utils/Utils.WinForm/DataGridViewHelper/Columns/DgvTextAndImageColumn.cs
@@ -0,0 +1,142 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Utils.WinForm.DataGridViewHelper
+{
+	[ToolboxBitmap(typeof(DataGridViewImageColumn))]
+	public class DgvTextAndImageColumn : DataGridViewTextBoxColumn
+	{
+		private Image imageValue;
+		private Size imageSize;
+
+		public sealed override DataGridViewCell CellTemplate
+		{
+			get => base.CellTemplate;
+			set => base.CellTemplate = value;
+		}
+
+		public DgvTextAndImageColumn()
+		{
+			CellTemplate = new DgvTextAndImageCell();
+		}
+
+		public Image Image
+		{
+			get => imageValue;
+			set
+			{
+				if (Equals(Image, value))
+					return;
+
+				imageValue = value;
+
+				if (imageValue != null)
+				{
+					imageSize = imageValue.Size;
+
+					if (InheritedStyle != null)
+					{
+						var inheritedPadding = InheritedStyle.Padding;
+						DefaultCellStyle.Padding = new Padding(imageSize.Width, inheritedPadding.Top, inheritedPadding.Right, inheritedPadding.Bottom);
+					}
+				}
+				else
+				{
+					DefaultCellStyle.Padding = new Padding(0);
+				}
+			}
+		}
+
+		private DgvTextAndImageCell TextAndImageCellTemplate => CellTemplate as DgvTextAndImageCell;
+
+		internal Size ImageSize => imageSize;
+
+		public override object Clone()
+		{
+			if (base.Clone() is DgvTextAndImageColumn c)
+			{
+				c.imageValue = imageValue;
+				c.imageSize = imageSize;
+				return c;
+			}
+
+			return null;
+		}
+	}
+
+	public class DgvTextAndImageCell : DataGridViewTextBoxCell
+	{
+		private Image imageValue;
+		private Size imageSize;
+
+		private DgvTextAndImageColumn OwningTextAndImageColumn => OwningColumn as DgvTextAndImageColumn;
+
+		public Image Image
+		{
+			get
+			{
+				if (OwningColumn == null || OwningTextAndImageColumn == null)
+					return imageValue;
+				else if (imageValue != null)
+					return imageValue;
+				else
+					return OwningTextAndImageColumn.Image;
+			}
+			set
+			{
+				if (Equals(imageValue, value))
+					return;
+
+				imageValue = value;
+
+				if (imageValue != null)
+				{
+					imageSize = imageValue.Size;
+
+					var inheritedPadding = InheritedStyle.Padding;
+					Style.Padding = new Padding(imageSize.Width + 2, inheritedPadding.Top, inheritedPadding.Right, inheritedPadding.Bottom);
+				}
+				else
+				{
+					Style.Padding = new Padding(0);
+				}
+			}
+		}
+
+		protected override void Paint(Graphics graphics, Rectangle clipBounds, Rectangle cellBounds, int rowIndex,
+			DataGridViewElementStates cellState, object value, object formattedValue, string errorText,
+			DataGridViewCellStyle cellStyle,
+			DataGridViewAdvancedBorderStyle advancedBorderStyle, DataGridViewPaintParts paintParts)
+		{
+			// Paint the base content
+			base.Paint(graphics, clipBounds, cellBounds, rowIndex, cellState,
+				value, formattedValue, errorText, cellStyle,
+				advancedBorderStyle, paintParts);
+
+			if (Image == null)
+				return;
+
+			// Draw the image clipped to the cell.
+			var container = graphics.BeginContainer();
+
+			var point = new Point(cellBounds.Location.X, cellBounds.Location.Y);
+			point.X = point.X + 2;
+			graphics.SetClip(cellBounds);
+			graphics.DrawImageUnscaled(Image, point);
+
+			graphics.EndContainer(container);
+		}
+
+		public override object Clone()
+		{
+			if (base.Clone() is DgvTextAndImageCell c)
+			{
+				c.imageValue = imageValue;
+				c.imageSize = imageSize;
+				return c;
+			}
+
+			return null;
+		}
+	}
+}
